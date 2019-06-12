5ba8df638c92afb8ffbbb4bffb9bb8bfb3115f5a
Felipe Ortiz <jfoc_link@hotmail.com>
2019-06-11 22:38:29 -0300

    Updated Bootstrap to version 4.3.2 Added Recipies/Index.cshtml Modified Views/Shared/_Layout.cshtml

--- RecipeBookApp/Controllers/RecipiesController.cs
+++ RecipeBookApp/Controllers/RecipiesController.cs
@@ -17,10 +17,29 @@ namespace RecipeBookApp.Controllers
         {
             _context = new ApplicationDbContext();
         }
+
+        //Se encargara de deshacerse del contexto de la app
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Recipies
         public ActionResult Index()
         {
-            return View();
+            //Inicializamos una variable que contendra las recetas de la base de datos
+            //Esto se logra, accediendo al atributo _context, luego a su contexto "Recipies" el cual responde a un DbSet<Recipe>
+            //Al hacer eso, no generara una query a la BD para obtener el contenido.
+            //A continuacion, existen dos formas, en diferido y delegando la responsabilidad de iterar en la vista
+            //O, al final de la instruccion, llamar al metodo
+            var recipies = _context.Recipes.ToList();
+
+            return View(recipies);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
